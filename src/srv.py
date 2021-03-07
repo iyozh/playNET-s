dc8c019a93ba8aece5d45975b2aafd5e6a6728cd
iyozh <evzhenko1106@gmail.com>
2021-03-08 00:47:28 +0300

    src rf

--- src/srv.py
+++ src/srv.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request
 from dynaconf import settings as _ds
 from flask_mail import Mail, Message
 
-from feedback_form import FeedBackForm
+from src.feedback_form import FeedBackForm
 
 app = Flask(__name__)
 
